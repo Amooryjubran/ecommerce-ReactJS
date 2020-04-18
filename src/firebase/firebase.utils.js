cbf2ddfea6da7bc45bfec2f724009a7d61edc137
amooryjubran <omarsalehjubran@gmail.com>
2020-04-18 12:09:27 -0400

    firebase-signUp-with-your-email

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,7 +13,30 @@ const config = {
     measurementId: "G-FM87GXDHWG"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
 
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShopt = await userRef.get();
+    
+    if(!snapShopt.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+    return userRef;
+}
 
 firebase.initializeApp(config);
 
